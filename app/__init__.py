48038e81bcc1c7366b52416ac7e83aff1c5539cf
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-07-11 13:48:47 +0800

    Change to __init__.py for future use

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app.main import app
+from app.__init__ import app
 
 if __name__ == "__main__":
 	app.run(debug=True)
