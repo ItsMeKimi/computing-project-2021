4ded50c408db876263b33ac39f4bd6f0ccceec7f
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-07-12 13:55:56 +0800

    Quick Update

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -11,6 +11,10 @@ def mysite_app(test_config=None):
     def page_login():
         return render_template('index.html')
 
+    @app.route('/login2/')
+    def page_login2():
+        return render_template('/login/index.html')
+
     @app.errorhandler(404)
     def not_found(error):
         return render_template('404.html'), 404
