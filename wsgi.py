52c3a922f924af7d52e3907a65b7dd78a3bd2518
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-07-15 05:42:53 +0800

    Quick Updates

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
+from flask.templating import render_template
 from myapp import mysite_app
 
 app = mysite_app()
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'), 404
