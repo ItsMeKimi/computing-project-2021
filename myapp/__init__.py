849a0a6260aed197062e6d38ecd9d12654c52139
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-07-22 14:43:43 +0800

    .

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('/404/404.html'), 404
 
 def internal_server_error(error):
-    return render_template('500.html'), 500
+    return render_template('/500/500.html'), 500
 
 def mysite_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     @app.route('/')
     def index():
-        return render_template('home.html') 
+        return render_template('/home/home.html') 
 
     @app.route('/login/')
     def page_login():
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,22 @@
 from flask.templating import render_template
 from myapp import mysite_app
+from flask_assets import Environment, Bundle
 
 app = mysite_app()
+
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('index.scss', filters='pyscss', output='all.css')
+
+assets.config['SECRET_KEY'] = 'secret!'
+
+assets.config['PYSCSS_LOAD_PATHS'] = assets.load_path
+assets.config['PYSCSS_STATIC_URL'] = assets.url
+assets.config['PYSCSS_STATIC_ROOT'] = assets.directory
+assets.config['PYSCSS_ASSETS_URL'] = assets.url
+assets.config['PYSCSS_ASSETS_ROOT'] = assets.directory
+
+assets.register('scss_all', scss)
 
 if __name__ == "__main__":
 	app.run(debug=True)
