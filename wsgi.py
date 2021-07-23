b0547584d83877a54fff7d218b8cc64fac030ae7
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-07-23 09:34:14 +0800

    .

--- wsgi.py
+++ wsgi.py
@@ -10,7 +10,6 @@ assets.url = app.static_url_path
 scss = Bundle('sass/index.scss', filters='pyscss', output='all.css')
 
 assets.config['SECRET_KEY'] = 'secret!'
-
 assets.config['PYSCSS_LOAD_PATHS'] = assets.load_path
 assets.config['PYSCSS_STATIC_URL'] = assets.url
 assets.config['PYSCSS_STATIC_ROOT'] = assets.directory
