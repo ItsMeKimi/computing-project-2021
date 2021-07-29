88eed960c42d3344cf644436f2bc6bf9e184642b
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-07-29 15:03:50 +0800

    .

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,11 @@
+from flask import Flask
 from flask.templating import render_template
 from myapp import mysite_app
-from flask_assets import Environment, Bundle
+import sass
 
 app = mysite_app()
 
-assets = Environment(app)
-assets.url = app.static_url_path
-
-scss = Bundle('sass/style.scss', filters='pyscss', output='css/style.css')
-
-assets.config['SECRET_KEY'] = 'secret!'
-assets.config['PYSCSS_LOAD_PATHS'] = assets.load_path
-assets.config['PYSCSS_STATIC_URL'] = assets.url
-assets.config['PYSCSS_STATIC_ROOT'] = assets.directory
-assets.config['PYSCSS_ASSETS_URL'] = assets.url
-assets.config['PYSCSS_ASSETS_ROOT'] = assets.directory
-
-assets.register('scss_all', scss)
+sass.compile(dirname=('myapp/static/sass', 'myapp/static/css'), output_style='compressed')
 
 if __name__ == "__main__":
 	app.run(debug=True)
