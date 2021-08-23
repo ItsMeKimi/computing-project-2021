ecbbe138a93ade9e016b8840f099697262451303
ItsMeKimi <yap.yuhung.kimi@dhs.sg>
2021-08-23 10:46:09 +0800

    Finalised Sign In Aspects ; Creating new cookie system based off g_state cookie to check user session

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from google.auth import crypt
+from google.auth import jwt
+from datetime import datetime
+
+def get_time():
+    _day = datetime.now().strftime('%d')
+    _month = datetime.now().strftime('%b')
+    _year = datetime.now().strftime('%Y')
+    _hour = datetime.now().strftime('%H')
+    _minute = datetime.now().strftime('%M')
+    _second = datetime.now().strftime('%S')
+    return f'{_day}/{_month}/{_year} {_hour}:{_minute}:{_second}'
+
+def get_requester_ip():
+    _ip_address = request.remote_addr
+    return _ip_address
 
 def page_not_found(error):
     return render_template('/404/404.html'), 404
@@ -13,23 +29,28 @@ def mysite_app(test_config=None):
     def index():
         return render_template('/home/home.html') 
 
-    @app.route('/login/')
-    def page_login():
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == "POST":
+            datafromjs = request.form['mydata']
+            decoded = jwt.decode(datafromjs, certs=None, verify=False)
+            print(f"{get_requester_ip()} - - [{get_time()}] {decoded}")
+        
         return render_template('/login/login.html')
 
-    @app.route('/my-account/')
+    @app.route('/my-account')
     def my_account():
         return render_template('/my-account/my-account.html')
     
-    @app.route('/about/')
+    @app.route('/about')
     def about():
         return render_template('/about/about.html')
 
-    @app.route('/contact/')
+    @app.route('/contact')
     def contact():
         return render_template('/contact/contact.html')
 
-    @app.route('/menu/')
+    @app.route('/menu')
     def menu():
         return render_template('/menu/menu.html')
 
